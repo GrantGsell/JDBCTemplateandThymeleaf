a5d65c3074527644f2ada4d832cb4289d525fcdd
GrantGsell <47372698+GrantGsell@users.noreply.github.com>
2022-04-27 15:06:49 -0400

    Added TeacherDao interface

---
+++ classroster/src/main/java/com/sg/classroster/dao/TeacherDao.java
@@ -0,0 +1,16 @@
+package com.sg.classroster.dao;
+
+import com.sg.classroster.dto.Teacher;
+import java.util.List;
+
+/**
+ *
+ * @author Grant
+ */
+public interface TeacherDao {
+    Teacher getTeacherById(int id);
+    List<Teacher> getAllTeachers();
+    Teacher addTeacher(Teacher teacher);
+    void updateTeacher(Teacher teacher);
+    void deleteTeacherById(int id);
+}
