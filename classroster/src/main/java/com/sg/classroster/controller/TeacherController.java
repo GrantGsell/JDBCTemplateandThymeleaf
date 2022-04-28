174d9efd50e5744352b033ecd3e3689c8fda9118
GrantGsell <47372698+GrantGsell@users.noreply.github.com>
2022-04-28 12:21:58 -0400

    Fixed pom dependencies, added teacher validation

--- classroster/src/main/java/com/sg/classroster/controller/TeacherController.java
+++ classroster/src/main/java/com/sg/classroster/controller/TeacherController.java
@@ -8,8 +8,13 @@ import com.sg.classroster.dao.CourseDao;
 import com.sg.classroster.dao.StudentDao;
 import com.sg.classroster.dao.TeacherDao;
 import com.sg.classroster.dto.Teacher;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -23,19 +28,22 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class TeacherController {
 
-   @Autowired
-   TeacherDao teacherDao;
-
-   @Autowired
-   StudentDao studentDao;
-
-   @Autowired
-   CourseDao courseDao;
-   
-   @GetMapping("teachers")
+    Set<ConstraintViolation<Teacher>> violations = new HashSet<>();
+    
+    @Autowired
+            TeacherDao teacherDao;
+    
+    @Autowired
+            StudentDao studentDao;
+    
+    @Autowired
+            CourseDao courseDao;
+    
+    @GetMapping("teachers")
     public String displayTeachers(Model model) {
         List<Teacher> teachers = teacherDao.getAllTeachers();
         model.addAttribute("teachers", teachers);
+        model.addAttribute("errors", violations);
         return "teachers";
     }
     
@@ -50,7 +58,12 @@ public class TeacherController {
         teacher.setLastName(lastName);
         teacher.setSpecialty(specialty);
         
-        teacherDao.addTeacher(teacher);
+        Validator validate = Validation.buildDefaultValidatorFactory().getValidator();
+        violations = validate.validate(teacher);
+        
+        if(violations.isEmpty()) {
+            teacherDao.addTeacher(teacher);
+        }
         
         return "redirect:/teachers";
     }
--- classroster/src/main/java/com/sg/classroster/dto/Teacher.java
+++ classroster/src/main/java/com/sg/classroster/dto/Teacher.java
@@ -1,10 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.sg.classroster.dto;
 
 import java.util.Objects;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 
 /**
  * @author Grant
@@ -13,8 +12,16 @@ import java.util.Objects;
  */
 public class Teacher {
     private int id;
+    
+    @NotBlank(message = "First name must not be empty.")
+    @Size(max = 30, message = "First name must be less than 30 characters.")
     private String firstName;
+    
+    @NotBlank(message = "Last name must not be empty.")
+    @Size(max = 50, message = "Last name must be less than 50 characters.")
     private String lastName;
+    
+    @Size(max = 50, message = "Specialty must be less than 50 characters")
     private String specialty;
 
     // Teacher setters
